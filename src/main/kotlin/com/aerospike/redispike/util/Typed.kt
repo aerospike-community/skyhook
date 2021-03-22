ba6e6cd964ab32f5db2e29d70e61b791acb76d44
yrizhkov <yrizhkov@aerospike.com>
2021-03-22 12:30:23 +0200

    add more utility methods

--- src/main/kotlin/com/aerospike/redispike/util/Typed.kt
+++ src/main/kotlin/com/aerospike/redispike/util/Typed.kt
@@ -15,4 +15,16 @@ object Typed {
         }
         return Value.StringValue(wireVal.toString(Charsets.UTF_8))
     }
+
+    fun getInteger(wireVal: ByteArray?): Int {
+        return String(wireVal!!).toInt()
+    }
+
+    fun getLong(wireVal: ByteArray?): Long {
+        return String(wireVal!!).toLong()
+    }
+
+    fun getDouble(wireVal: ByteArray?): Double {
+        return String(wireVal!!).toDouble()
+    }
 }
