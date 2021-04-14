94ebf7fe079cf7bc62aa3c37a51ba4044948c4be
yrizhkov <yrizhkov@aerospike.com>
2021-04-14 12:16:21 +0300

    add InfoUtils

---
+++ src/main/kotlin/com/aerospike/skyhook/util/InfoUtils.kt
@@ -0,0 +1,23 @@
+package com.aerospike.skyhook.util
+
+import com.aerospike.client.Info
+import com.aerospike.client.cluster.Node
+import java.util.regex.Pattern
+
+object InfoUtils {
+
+    fun getNamespaceInfo(ns: String, node: Node): Map<String, String> {
+        val schemaInfo = Info.request(null, node, "namespace/$ns")
+        return schemaInfo.split(";")
+            .map { it.split("=".toRegex(), 2).toPair() }
+            .toMap()
+    }
+
+    fun getSetInfo(ns: String, set: String?, node: Node): Map<String, String> {
+        val sets = Info.request(null, node, "sets")
+        val tableInfo = sets.split(";")
+            .filter { it.startsWith("ns=$ns:set=$set") }[0]
+        return Pattern.compile("\\s*:\\s*").split(tableInfo).toList()
+            .filterNotNull().map { it.split("=".toRegex(), 2).toPair() }.toMap()
+    }
+}
